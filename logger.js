1fcbcc9041654b0f7c6dd50d36dfd73aaf4ec81c
BoongMai <thanhduypham9@gmail.com>
2022-02-23 08:52:12 +0700

    Initial commit

---
+++ logger.js
@@ -0,0 +1,12 @@
+export default function logger(reducer) {
+    return (prevState, action, args) => {
+        console.group(action)
+        console.log('PrevState: ',prevState);
+        console.log('args: ',args);
+        const nextState = reducer(prevState, action, args)
+        console.log('Next state: ', nextState)
+
+        console.groupEnd()
+        return nextState
+    }
+}
